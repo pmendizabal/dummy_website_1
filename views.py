d51bcf1207dbe7c415c27b32ee0542d88cc3ff64
newkeynesian <raultirado@hotmail.com>
2024-09-12 00:56:59 -0600

    C-version commit

--- views.py
+++ views.py
@@ -1,7 +1,23 @@
 from flask import Blueprint, render_template
 
-views = Blueprint(__name__, "views")
+views = Blueprint(__name__, "home")
 
-@views.route("/")
+@views.route("/home/")
 def home():
     return render_template("index.html")
+
+@views.route("/cadenas/")
+def cadena_valor():
+    return render_template("cadenas.html")
+
+@views.route("/capacitacion/")
+def capacitacion():
+    return render_template("capacitacion.html")
+
+@views.route("/financiamiento/")
+def financiamiento():
+    return render_template("fin.html")
+
+@views.route("/tesoreria/")
+def tesoreria():
+    return render_template("teso.html")
